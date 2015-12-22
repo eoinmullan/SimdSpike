d670878a9d516dac9fc08979eceb6ef3136bcc0f
eoinmullan <eoinmullan@hotmail.com>
2015-12-22 16:01:22 +0000

    Adding performance tests for both floats and ushorts & some refactoring.

--- SimdSpike/Program.cs
+++ SimdSpike/Program.cs
@@ -1,108 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
+﻿using System.Numerics;
 using static System.Console;
-using static SimdSpike.MethodValidation;
 using static SimdSpike.Utilities;
-using Stopwatch = NUnit.Framework.Compatibility.Stopwatch;
 
 namespace SimdSpike {
     internal class Program {
-        private const int TestSetSize = 20000000;
+        private const int TestSetSize = 8294400;
 
         static void Main() {
             if (!Vector.IsHardwareAccelerated) {
                 WriteLine("Hardware acceleration not supported.");
                 return;
             }
+            Write("Hardware acceleration is supported");
 
             Write("Validating addition methods...");
-            ValidateAdditionMethods(NaiveSum, HwAcceleratedSum);
-            ValidateAdditionInPlaceMethods(NaiveSumInPlace, HwAcceleratedSumInPlace);
-            ValidateAdditionFuncs(NaiveSumFunc);
+            FloatSimdProcessor.ValidateAdditionMethods();
             WriteLine(" methods correct.");
 
-            Write("Hardware acceleration is supported, generating test data...");
-            var one = Enumerable.Range(0, TestSetSize).Select(x => RandomFloat()).ToArray();
-            var two = Enumerable.Range(0, TestSetSize).Select(x => RandomFloat()).ToArray();
-            WriteLine(" done.");
-
-            WriteLine("Performance testing...");
-            var sw = new Stopwatch();
-            var naiveTimesMs = new List<long>();
-            var hwTimesMs = new List<long>();
-            for (var i = 0; i < 5; i++) {
-                var naiveSum = new float[one.Length];
-                sw.Restart();
-                NaiveSum(one, two, naiveSum);
-                var naiveTimeMs = sw.ElapsedMilliseconds;
-                naiveTimesMs.Add(naiveTimeMs);
-                WriteLine($"Naive addition took:          {naiveTimeMs}ms (last value = {naiveSum[naiveSum.Length-1]}).");
-
-                var hwSum = new float[one.Length];
-                sw.Restart();
-                HwAcceleratedSum(one, two, hwSum);
-                var hwTimeMs = sw.ElapsedMilliseconds;
-                hwTimesMs.Add(hwTimeMs);
-                WriteLine($"HW accelerated addition took: {hwTimeMs}ms (last value = {hwSum[hwSum.Length - 1]}).");
-            }
-
-            WriteLine();
-            WriteLine($"Naive method average time:          {naiveTimesMs.Average()}");
-            WriteLine($"HW accelerated method average time: {hwTimesMs.Average()}");
-        }
-
-        private static void NaiveSum(float[] lhs, float[] hs, float[] result) {
-            var length = lhs.Length;
-            for (var i = 0; i < length; ++i) {
-                result[i] = lhs[i] + hs[i];
-            }
-        }
-
-        private static void NaiveSumInPlace(float[] lhs, float[] rhs) {
-            var length = lhs.Length;
-            for (var i = 0; i < length; ++i) {
-                lhs[i] += rhs[i];
-            }
-        }
-
-        private static float[] NaiveSumFunc(float[] lhs, float[] hs) {
-            var length = lhs.Length;
-            var result = new float[length];
-            for (var i = 0; i < length; ++i) {
-                result[i] = lhs[i] + hs[i];
-            }
-            return result;
-        }
-
-        private static void HwAcceleratedSum(float[] lhs, float[] rhs, float[] result) {
-            var simdLength = Vector<float>.Count;
-            int i;
-            for (i = 0; i < lhs.Length - simdLength; i += simdLength) {
-                var va = new Vector<float>(lhs, i);
-                var vb = new Vector<float>(rhs, i);
-                va += vb;
-                va.CopyTo(result, i);
-            }
-
-            for (; i < lhs.Length; ++i) {
-                result[i] = lhs[i] + rhs[i];
-            }
-        }
-
-        private static void HwAcceleratedSumInPlace(float[] lhs, float[] rhs) {
-            int simdLength = Vector<float>.Count;
-            int i = 0;
-            for (i = 0; i < lhs.Length - simdLength; i += simdLength) {
-                Vector<float> va = new Vector<float>(lhs, i);
-                Vector<float> vb = new Vector<float>(rhs, i);
-                va += vb;
-                va.CopyTo(lhs, i);
-            }
-            for (; i < lhs.Length; ++i) {
-                lhs[i] += rhs[i];
-            }
+            PrintSimdEffectiveness();
+            
+            FloatSimdProcessor.TestInPlaceAddition(TestSetSize);
+            UShortSimdProcessor.TestInPlaceAddition(TestSetSize);
         }
     }
 }
--- SimdSpike/Utilities.cs
+++ SimdSpike/Utilities.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Numerics;
+using static System.Console;
 
 namespace SimdSpike {
     public static class Utilities {
@@ -8,6 +10,25 @@ namespace SimdSpike {
             var mantissa = (random.NextDouble() * 2.0) - 1.0;
             var exponent = Math.Pow(2.0, random.Next(-126, 128));
             return (float)(mantissa * exponent);
+        }
+
+        public static ushort RandomUShort() {
+            return (ushort)random.Next(ushort.MinValue, ushort.MaxValue);
+        }
+
+        public static void PrintSimdEffectiveness() {
+            WriteLine($"Simd register is {Vector<int>.Count * sizeof(int)} bytes");
+            try { WriteLine($"{Vector<byte>.Count} bytes per operation"); } catch { WriteLine($"Simd not supported for byte"); }
+            try { WriteLine($"{Vector<char>.Count} chars per operation"); } catch { WriteLine($"Simd not supported for char"); }
+            try { WriteLine($"{Vector<float>.Count} floats per operation"); } catch { WriteLine($"Simd not supported for float"); }
+            try { WriteLine($"{Vector<short>.Count} shorts per operation"); } catch { WriteLine($"Simd not supported for short"); }
+            try { WriteLine($"{Vector<ushort>.Count} ushorts per operation"); } catch { WriteLine($"Simd not supported for ushort"); }
+            try { WriteLine($"{Vector<int>.Count} ints per operation"); } catch { WriteLine($"Simd not supported for int"); }
+            try { WriteLine($"{Vector<uint>.Count} uints per operation"); } catch { WriteLine($"Simd not supported for uint"); }
+            try { WriteLine($"{Vector<long>.Count} longs per operation"); } catch { WriteLine($"Simd not supported for long"); }
+            try { WriteLine($"{Vector<ulong>.Count} ulongs per operation"); } catch { WriteLine($"Simd not supported for ulong"); }
+            try { WriteLine($"{Vector<double>.Count} doubles per operation"); } catch { WriteLine($"Simd not supported for double"); }
+            try { WriteLine($"{Vector<decimal>.Count} decimals per operation"); } catch { WriteLine($"Simd not supported for decimal"); }
         }
     }
 }

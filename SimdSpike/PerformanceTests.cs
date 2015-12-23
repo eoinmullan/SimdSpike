599ae09b0a95a53932d3e99350edfcc6bc2a7c79
eoinmullan <eoinmullan@hotmail.com>
2015-12-23 14:54:04 +0000

    Simple rename/refactoring.

--- SimdSpike/PerformanceTests.cs
+++ SimdSpike/PerformanceTests.cs
@@ -8,38 +8,39 @@ using static SimdSpike.Utilities;
 
 namespace SimdSpike {
     public class PerformanceTests {
+        private static readonly Stopwatch stopwatch = new Stopwatch();
+
         public static void TestInPlaceFloatAddition(int testSetSize) {
             WriteLine();
             Write("Testing floats, generating test data...");
             var floatsOne = Enumerable.Range(0, testSetSize).Select(x => RandomFloat()).ToArray();
             var floatsTwo = Enumerable.Range(0, testSetSize).Select(x => RandomFloat()).ToArray();
             WriteLine(" done, testing...");
-
-            var sw = new Stopwatch();
+            
             var naiveTimesMs = new List<long>();
             var hwTimesMs = new List<long>();
             for (var i = 0; i < 3; i++) {
                 var floatsOneCopy = new float[floatsOne.Length];
 
                 floatsOne.CopyTo(floatsOneCopy, 0);
-                sw.Restart();
+                stopwatch.Restart();
                 HwAcceleratedSumInPlace(floatsOneCopy, floatsTwo);
-                var hwTimeMs = sw.ElapsedMilliseconds;
+                var hwTimeMs = stopwatch.ElapsedMilliseconds;
                 hwTimesMs.Add(hwTimeMs);
                 WriteLine($"HW accelerated addition took: {hwTimeMs}ms (last value = {floatsOneCopy[floatsOneCopy.Length - 1]}).");
 
                 floatsOne.CopyTo(floatsOneCopy, 0);
-                sw.Restart();
+                stopwatch.Restart();
                 NaiveSumInPlace(floatsOneCopy, floatsTwo);
-                var naiveTimeMs = sw.ElapsedMilliseconds;
+                var naiveTimeMs = stopwatch.ElapsedMilliseconds;
                 naiveTimesMs.Add(naiveTimeMs);
                 WriteLine($"Naive addition took:          {naiveTimeMs}ms (last value = {floatsOneCopy[floatsOneCopy.Length - 1]}).");
             }
 
-            WriteLine("Testing floats");
+            WriteLine("Testing float array addition");
             WriteLine($"Naive method average time:          {naiveTimesMs.Average()}");
             WriteLine($"HW accelerated method average time: {hwTimesMs.Average()}");
-            WriteLine($"Hardware speedup:                   {(naiveTimesMs.Average() / hwTimesMs.Average()) * 100:0.00}%");
+            WriteLine($"Hardware speedup:                   {naiveTimesMs.Average() / hwTimesMs.Average():P}%");
         }
 
         public static void TestInPlaceUShortAddition(int testSetSize) {
@@ -48,32 +49,31 @@ namespace SimdSpike {
             var ushortsOne = GetRandomUShortArray(testSetSize);
             var ushortsTwo = GetRandomUShortArray(testSetSize);
             WriteLine(" done, testing...");
-
-            var sw = new Stopwatch();
+            
             var naiveTimesMs = new List<long>();
             var hwTimesMs = new List<long>();
             for (var i = 0; i < 3; i++) {
                 var ushortsOneCopy = new ushort[ushortsOne.Length];
 
                 ushortsOne.CopyTo(ushortsOneCopy, 0);
-                sw.Restart();
+                stopwatch.Restart();
                 HwAcceleratedSumInPlaceUnchecked(ushortsOneCopy, ushortsTwo);
-                var hwTimeMs = sw.ElapsedMilliseconds;
+                var hwTimeMs = stopwatch.ElapsedMilliseconds;
                 hwTimesMs.Add(hwTimeMs);
                 WriteLine($"HW accelerated addition took: {hwTimeMs}ms (last value = {ushortsOneCopy[ushortsOneCopy.Length - 1]}).");
 
                 ushortsOne.CopyTo(ushortsOneCopy, 0);
-                sw.Restart();
+                stopwatch.Restart();
                 NaiveSumInPlaceUnchecked(ushortsOneCopy, ushortsTwo);
-                var naiveTimeMs = sw.ElapsedMilliseconds;
+                var naiveTimeMs = stopwatch.ElapsedMilliseconds;
                 naiveTimesMs.Add(naiveTimeMs);
                 WriteLine($"Naive addition took:          {naiveTimeMs}ms (last value = {ushortsOneCopy[ushortsOneCopy.Length - 1]}).");
             }
 
-            WriteLine("Testing floats");
+            WriteLine("Testing ushort array addition");
             WriteLine($"Naive method average time:          {naiveTimesMs.Average()}");
             WriteLine($"HW accelerated method average time: {hwTimesMs.Average()}");
-            WriteLine($"Hardware speedup:                   {(naiveTimesMs.Average() / hwTimesMs.Average()) * 100:0.00}%");
+            WriteLine($"Hardware speedup:                   {naiveTimesMs.Average() / hwTimesMs.Average():P}%");
         }
     }
 }

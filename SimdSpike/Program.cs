66202b31e33c3632f4b38f0384e7546832add7a1
eoinmullan <eoinmullan@hotmail.com>
2015-12-23 14:54:44 +0000

    Adding performance test for min/max functions.

--- SimdSpike/Program.cs
+++ SimdSpike/Program.cs
@@ -18,6 +18,7 @@ namespace SimdSpike {
             
             TestInPlaceFloatAddition(EightKUltraHDRes);
             TestInPlaceUShortAddition(EightKUltraHDRes);
+            TestMinMaxFunctions(EightKUltraHDRes);
         }
     }
 }

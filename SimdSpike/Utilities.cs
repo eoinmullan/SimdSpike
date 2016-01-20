47785224dccfe7a9c12ea7c7ccfde54ed05902f4
eoinmullan <eoinmullan@hotmail.com>
2016-01-20 08:49:19 +0000

    Adding some more detail to the output.

--- SimdSpike/Utilities.cs
+++ SimdSpike/Utilities.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Numerics;
+using System.Security.Cryptography;
 using static System.Console;
 
 namespace SimdSpike {
@@ -27,17 +28,17 @@ namespace SimdSpike {
 
         public static void PrintHardwareSpecificSimdEffectiveness() {
             WriteLine($"Simd register is {Vector<int>.Count * sizeof(int)} bytes");
-            try { WriteLine($"{Vector<byte>.Count} bytes per operation"); } catch { WriteLine($"Simd not supported for byte"); }
-            try { WriteLine($"{Vector<char>.Count} chars per operation"); } catch { WriteLine($"Simd not supported for char"); }
-            try { WriteLine($"{Vector<float>.Count} floats per operation"); } catch { WriteLine($"Simd not supported for float"); }
-            try { WriteLine($"{Vector<short>.Count} shorts per operation"); } catch { WriteLine($"Simd not supported for short"); }
-            try { WriteLine($"{Vector<ushort>.Count} ushorts per operation"); } catch { WriteLine($"Simd not supported for ushort"); }
-            try { WriteLine($"{Vector<int>.Count} ints per operation"); } catch { WriteLine($"Simd not supported for int"); }
-            try { WriteLine($"{Vector<uint>.Count} uints per operation"); } catch { WriteLine($"Simd not supported for uint"); }
-            try { WriteLine($"{Vector<long>.Count} longs per operation"); } catch { WriteLine($"Simd not supported for long"); }
-            try { WriteLine($"{Vector<ulong>.Count} ulongs per operation"); } catch { WriteLine($"Simd not supported for ulong"); }
-            try { WriteLine($"{Vector<double>.Count} doubles per operation"); } catch { WriteLine($"Simd not supported for double"); }
-            try { WriteLine($"{Vector<decimal>.Count} decimals per operation"); } catch { WriteLine($"Simd not supported for decimal"); }
+            try { WriteLine($"{Vector<byte>.Count} bytes ({sizeof(byte)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for byte"); }
+            try { WriteLine($"{Vector<char>.Count} chars ({sizeof(char)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for char"); }
+            try { WriteLine($"{Vector<float>.Count} floats ({sizeof(float)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for float"); }
+            try { WriteLine($"{Vector<short>.Count} shorts ({sizeof(short)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for short"); }
+            try { WriteLine($"{Vector<ushort>.Count} ushorts ({sizeof(ushort)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for ushort"); }
+            try { WriteLine($"{Vector<int>.Count} ints ({sizeof(int)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for int"); }
+            try { WriteLine($"{Vector<uint>.Count} uints ({sizeof(uint)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for uint"); }
+            try { WriteLine($"{Vector<long>.Count} longs ({sizeof(long)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for long"); }
+            try { WriteLine($"{Vector<ulong>.Count} ulongs ({sizeof(ulong)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for ulong"); }
+            try { WriteLine($"{Vector<double>.Count} doubles ({sizeof(double)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for double"); }
+            try { WriteLine($"{Vector<decimal>.Count} decimals ({sizeof(decimal)} bytes each) per operation"); } catch { WriteLine($"Simd not supported for decimal"); }
         }
     }
 }
